a20257fef314d53270934e9efb66abcd301c2f07
xiangtai <xiangtai@xiangtaideMac-mini.local>
2015-06-24 13:24:45 +0800

    Initial Commit

---
+++ 自定义UICollectionView布局/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义UICollectionView布局
+//
+//  Created by xiangtai on 15/6/24.
+//  Copyright (c) 2015年 何凯楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义UICollectionView布局/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义UICollectionView布局
+//
+//  Created by xiangtai on 15/6/24.
+//  Copyright (c) 2015年 何凯楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
